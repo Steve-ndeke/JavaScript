e899ce69ffaeb7e7ac6d638d19483bcbd116d9a4
Steve-ndeke <steve.ndeke1@accesscfa.com>
2022-08-31 16:30:48 +0300

    Arithmetics operations

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.write("Welcome to this page");
 alert("hello");
 console.log("It worked")
+
+
+//Simple arithmetics
+var myString = "s"+" "+"e";
+console.log(myString);
+
+//increamenting a given number
+var b = 5;
+console.log(b);
+b++;
+console.log(b);
+
+//Decreamenting
+b--;
+console.log(b);
+
+//addition and modulus
+var d = 4;
+var r = 3;
+
+var total = d+r;
+var rem = d % 3
+console.log(total)
+console.log(rem)
